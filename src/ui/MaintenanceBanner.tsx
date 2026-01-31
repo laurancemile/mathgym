a3c81175f7777e288ea2633f6f02ccbdca4caac1
Laurance Mile <85408797+laurancemile@users.noreply.github.com>
2026-01-31 11:47:30 +0200

    Add MaintenanceBanner to the main application
    Implement MaintenanceBanner component in the App.

---
+++ src/ui/MaintenanceBanner.tsx
@@ -0,0 +1,28 @@
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
+import MaintenanceBanner from "./ui/MaintananceBanner";
+
+const App = () => {
+	return (
+		<>
+			<MaintenanceBanner />
+			<Home />
+			<Toaster
+				position="top-center"
+				reverseOrder={false}
+				gutter={8}
+				containerStyle={{
+					fontFamily: "sans-serif",
+					fontSize: "16px",
+				}}
+				toasterId="default"
+				toastOptions={{
+					success: { duration: 5000 },
+					error: { duration: 5000 },
+				}}
+			/>
+		</>
+	);
+};
+
+export default App;
